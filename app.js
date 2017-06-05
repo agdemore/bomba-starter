e57ef96ab77deef0302fab3d6a40ca6c3c659a16
forwardmomentum <empirefallstechnologies@gmail.com>
2017-06-05 20:08:23 +0300

    web3 injection

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ var port = 8000;
 var express = require('express');
 var app = express();
 var path = require('path');
+var Web3 = require('web3');
 
+var web3 = new Web3();
 
 app.use(express.static(path.join(__dirname, 'web/public')));
 app.use('/dist', express.static(path.join(__dirname, 'web/dist')));
 app.get(function root(req, res) {
   res.sendFile(path.join(__dirname, 'web/public/index.html'));
 });
+
 app.use('/test', function root(req, res) {
+
+  console.log(web3);
+
   res.json({ 'eeeee': 'pooookkkk' });
 });
+
 app.use('/test2', function root(req, res) {
   res.json({ 'eeeee222': 'pooookkkk2222' });
 });
