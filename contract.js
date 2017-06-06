9f1137d213c4c130f97bf8a5c048117532bfd8ac
agdemore <agdemore@gmail.com>
2017-06-06 20:24:29 +0300

    add mongo

--- contract.js
+++ contract.js
@@ -15,7 +15,7 @@ let bills = [
   {
     id: "111",
     type: "open",
-    clientData: JSON.stringify({name: 'alex', pay: 1000}),
+    clientData: JSON.stringify({id: 111, title: 'check', pay: 1000}),
     summ: 10,
     payers: ["0x75dCd745c79Ba87cE88C328b887Ee8A082B7c109", "0x49f2C47f1470989f4E0E8E3aa482f160A76Ee2Aa"],
     receiver: "0x1d7ED9A6f43C697d365715759f3281C0bc67EcfC"
@@ -23,7 +23,7 @@ let bills = [
   {
     id: "222",
     type: "open",
-    clientData: JSON.stringify({name: 'alex', pay: 1000}),
+    clientData: JSON.stringify({id: 222, title: 'check', pay: 1000}),
     summ: 7,
     payers: ["0x1d7ED9A6f43C697d36571575", "0x75dCd745c79Ba87cE88C328b887Ee8A082B7c109"],
     receiver: "0x49f2C47f1470989f4E0E8E3aa482f160A76Ee2Aa"
@@ -32,7 +32,7 @@ let bills = [
     id: "333",
     summ: 13,
     type: "closing",
-    clientData: JSON.stringify({name: 'alex', pay: 1000}),
+    clientData: JSON.stringify({id: 333, title: 'check', pay: 1000}),
     payers: ["0x1d7ED9A6f43C697d365715759f3281C0bc67EcfC", "0x75dCd745c79Ba87cE88C328b887Ee8A082B7c109"],
     receiver: "0x49f2C47f1470989f4E0E8E3aa482f160A76Ee2Aa",
     pays: [
