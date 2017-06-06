fe6d7ccab00500900514823219e0b48b17b02cfe
forwardmomentum <empirefallstechnologies@gmail.com>
2017-06-06 14:42:21 +0300

    going to be HOT HOT HOT

--- contract.js
+++ contract.js
@@ -10,32 +10,79 @@ web3.setProvider(new web3.providers.HttpProvider('http://192.168.72.233:8545'));
 const abi = [{"constant":false,"inputs":[{"name":"receiver","type":"address"},{"name":"amount","type":"uint256"}],"name":"sendCoin","outputs":[{"name":"sufficient","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"coinBalanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"supply","type":"uint256"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"sender","type":"address"},{"indexed":false,"name":"receiver","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"CoinTransfer","type":"event"}];
 const contractAddress = "0xcfcf3bc7ff72012d57ff642d1defb895fb786b86";
 
+let bills = [
+  {
+    id: "111",
+    type: "open",
+    clientData: "",
+    summ: 10,
+    payers: ["0x75dCd745c79Ba87cE88C328b887Ee8A082B7c109", "0x49f2C47f1470989f4E0E8E3aa482f160A76Ee2Aa"],
+    receiver: "0x1d7ED9A6f43C697d365715759f3281C0bc67EcfC"
+  },
+  {
+    id: "222",
+    type: "open",
+    clientData: "",
+    summ: 7,
+    payers: ["0x1d7ED9A6f43C697d365715759f3281C0bc67EcfC", "0x75dCd745c79Ba87cE88C328b887Ee8A082B7c109"],
+    receiver: "0x49f2C47f1470989f4E0E8E3aa482f160A76Ee2Aa"
+  },
+  {
+    id: "333",
+    summ: 13,
+    type: "closing",
+    clientData: "",
+    payers: ["0x1d7ED9A6f43C697d365715759f3281C0bc67EcfC", "0x75dCd745c79Ba87cE88C328b887Ee8A082B7c109"],
+    receiver: "0x49f2C47f1470989f4E0E8E3aa482f160A76Ee2Aa",
+    pays: [
+      {
+        wallet: "0x1d7ED9A6f43C697d365715759f3281C0bc67EcfC",
+        amount: 8,
+        confirmed: false
+      },
+      {
+        wallet: "0x75dCd745c79Ba87cE88C328b887Ee8A082B7c109",
+        amount: 5,
+        confirmed: true
+      }
+    ]
+  }
+];
+
+
 /**
  * Записать новый счет в контракт
  */
-exports.registerBill = (bill) => {
-  // todo записать bill в контракт
+exports.saveOpenBill = (bill) => {
+  // а ниче не делает
 };
+
 
 /**
  *
  */
-exports.enterToBill = (accountId) => {
+exports.enterToBill = (accountId, sum) => {
   // todo осуществить перевод денег из входящего в сделку аккаунта на смарт контракт
+  // вернет корректное ли вхождение или нет
+  return true;
 };
 
 /**
  * получить счета в которых фигурирует данный аккаунт
  */
-exports.getBillsByAccount = () => {
-  // todo запросить у контракта билли связанные с аккаунтом (в любой роли)
+exports.getBillsByAccount = (accountWallet) => {
+  return bills.filter((bill) => {
+    if (bill.payers.indexOf(accountWallet) > -1) return true;
+    if (bill.receiver === accountWallet) return true;
+    return false;
+  });
 };
 
 /**
  * получить счет по id
  */
 exports.getBillById = (billId) => {
-  // todo запросить билль у контракта по billId
+  return bills.filter((bill) => {return bill.id === billId;})[0];
 };
 
 exports.getMainAccBalance = function test() {
