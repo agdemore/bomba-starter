76899528dd98433bc591ae27d2d303152597d4f3
forwardmomentum <empirefallstechnologies@gmail.com>
2017-06-06 13:24:09 +0300

    dfsfs

--- web/index.js
+++ web/index.js
@@ -1,21 +1,21 @@
-var webpack = require('webpack'),
-  webpackconfig = require('./webpack.config.js'),
-  webpackcompiler = webpack(webpackconfig);
-
-var port = 8000;
-var express = require('express');
-var app = express();
-var path = require('path');
-
-
-app.use(express.static(path.join(__dirname, '/public')));
-app.use('/dist', express.static(path.join(__dirname, '/dist')));
-app.get(function root(req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
-
-var server = app.listen(port, function() {
-  var host = server.address().address;
-  console.log('Server is listening at http://%s:%s', host, port);
-});
+// var webpack = require('webpack'),
+//   webpackconfig = require('./webpack.config.js'),
+//   webpackcompiler = webpack(webpackconfig);
+//
+// var port = 8000;
+// var express = require('express');
+// var app = express();
+// var path = require('path');
+//
+//
+// app.use(express.static(path.join(__dirname, '/public')));
+// app.use('/dist', express.static(path.join(__dirname, '/dist')));
+// app.get(function root(req, res) {
+//   res.sendFile(path.join(__dirname, '/public/index.html'));
+// });
+//
+//
+// var server = app.listen(port, function() {
+//   var host = server.address().address;
+//   console.log('Server is listening at http://%s:%s', host, port);
+// });
--- web/routes/auth.js
+++ web/routes/auth.js
@@ -6,11 +6,12 @@
 const express = require('express'),
     router = express.Router(),
     _ = require('lodash'),
+    db = require('../server/db'),
     moment = require('moment'),
     uuid = require('node-uuid');
 
-import helper from '../server/helper';
-import db from '../server/db';
+// import helper from '././server/helper';
+// import db from '../server/db';
 
 router.post('/', (req, res, next) => {
         let data = req.body;
