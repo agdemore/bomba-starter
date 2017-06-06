6222a87cb6be0c8a19ac0092f6bdf56da24c21fc
forwardmomentum <empirefallstechnologies@gmail.com>
2017-06-06 13:41:22 +0300

    todos

--- contract.js
+++ contract.js
@@ -1,23 +1,59 @@
 /**
  * Created by forwardmomentum on 05.06.17.
- *
  * API для вызовов на контракте-регуляторе
- *
  */
 
-var Web3 = require('web3');
-var web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://192.168.72.233:8545')); // адрес открытой к общению ноды нашей знойной сети эфира
+let Web3 = require('web3');
+let web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider('http://192.168.72.233:8545'));
 
-var abi = [{"constant":false,"inputs":[{"name":"receiver","type":"address"},{"name":"amount","type":"uint256"}],"name":"sendCoin","outputs":[{"name":"sufficient","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"coinBalanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"supply","type":"uint256"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"sender","type":"address"},{"indexed":false,"name":"receiver","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"CoinTransfer","type":"event"}];
-var contractAddress = "0xcfcf3bc7ff72012d57ff642d1defb895fb786b86";
+const abi = [{"constant":false,"inputs":[{"name":"receiver","type":"address"},{"name":"amount","type":"uint256"}],"name":"sendCoin","outputs":[{"name":"sufficient","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"coinBalanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"supply","type":"uint256"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"sender","type":"address"},{"indexed":false,"name":"receiver","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"CoinTransfer","type":"event"}];
+const contractAddress = "0xcfcf3bc7ff72012d57ff642d1defb895fb786b86";
 
 /**
- * Main account probably is producer of money
+ * Записать новый счет в контракт
  */
+exports.registerBill = (bill) => {
+  // todo записать bill в контракт
+};
+
+/**
+ *
+ */
+exports.enterToBill = (accountId) => {
+  // todo осуществить перевод денег из входящего в сделку аккаунта на смарт контракт
+};
+
+/**
+ * получить счета в которых фигурирует данный аккаунт
+ */
+exports.getBillsByAccount = () => {
+  // todo запросить у контракта билли связанные с аккаунтом (в любой роли)
+};
+
+/**
+ * получить счет по id
+ */
+exports.getBillById = (billId) => {
+  // todo запросить билль у контракта по billId
+};
+
 exports.getMainAccBalance = function test() {
   return web3.eth.getBalance(web3.eth.coinbase).toNumber();
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 exports.doSomethingWithContract = function() {
   var tokensContract = web3.eth.contract(abi).at(contractAddress);
