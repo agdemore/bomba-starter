db2be48dcb16e24329de5c81c73b9148c9ba96b0
forwardmomentum <empirefallstechnologies@gmail.com>
2017-06-07 13:37:30 +0300

    wip

--- web/app/modules/tabs/current.vue
+++ web/app/modules/tabs/current.vue
@@ -6,11 +6,63 @@
     overflow-y: auto;
   }
   .wrapper {
+    display: flex;
+    flex-direction: column;
+    flex-shrink: 0;
+    .spinner {
+      align-self: center;
+      margin-top: 11px;
+      width: 150px;
+      height: 150px;
+      cursor: pointer;
+
+      position: relative;
+    }
+
+    .double-bounce1, .double-bounce2 {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #2ecc71;
+      opacity: 0.6;
+      position: absolute;
+      top: 0;
+      left: 0;
+      
+      -webkit-animation: sk-bounce 2.0s infinite ease-in-out;
+      animation: sk-bounce 2.0s infinite ease-in-out;
+    }
+
+    .double-bounce2 {
+      -webkit-animation-delay: -1.0s;
+      animation-delay: -1.0s;
+    }
+
+    @-webkit-keyframes sk-bounce {
+      0%, 100% { -webkit-transform: scale(0.0) }
+      50% { -webkit-transform: scale(1.0) }
+    }
+
+    @keyframes sk-bounce {
+      0%, 100% { 
+        transform: scale(0.0);
+        -webkit-transform: scale(0.0);
+      } 50% { 
+        transform: scale(1.0);
+        -webkit-transform: scale(1.0);
+      }
+    }
+
   }
+  
 </style>
 <template>
   <div class="current">
     <div class="wrapper">
+      <div v-if="(bills || []).length === 0" class="spinner">
+        <div class="double-bounce1"></div>
+        <div class="double-bounce2"></div>
+      </div>
       <bill v-for="bill in bills"
             :key="bill.id"
             :bill="bill"
