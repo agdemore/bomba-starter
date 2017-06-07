e889aef1c9cd91ae8797cf792dbec19c4e79c88e
forwardmomentum <empirefallstechnologies@gmail.com>
2017-06-07 11:27:22 +0300

    sdf

--- contract.js
+++ contract.js
@@ -9,49 +9,27 @@ let web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://192.168.72.233:8545'));
 
 
-const abi = [{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"bills","outputs":[{"name":"id","type":"uint256"},{"name":"status","type":"uint256"},{"name":"reciever","type":"address"},{"name":"summ","type":"uint256"},{"name":"clientData","type":"string"},{"name":"payers","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"id","type":"uint256"}],"name":"getBillById","outputs":[{"name":"","type":"uint256"},{"name":"","type":"address"},{"name":"","type":"uint256"},{"name":"","type":"string"},{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"id","type":"uint256"},{"name":"reciever","type":"address"},{"name":"summ","type":"uint256"},{"name":"clientData","type":"string"},{"name":"payers","type":"string"}],"name":"saveOpenBill","outputs":[],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"}];
-const contractAddress = "0xb22e5bf814b3e2d81c6ea08b8f2cdebe5e46714c";
+const abi = [{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"bills","outputs":[{"name":"id","type":"uint256"},{"name":"status","type":"uint256"},{"name":"reciever","type":"address"},{"name":"summ","type":"uint256"},{"name":"clientData","type":"string"},{"name":"payers","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"id","type":"uint256"}],"name":"getPayStatuses","outputs":[{"name":"","type":"int256[10]"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"id","type":"uint256"}],"name":"getBillById","outputs":[{"name":"","type":"uint256"},{"name":"","type":"address"},{"name":"","type":"uint256"},{"name":"","type":"string"},{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"id","type":"uint256"}],"name":"getAmounts","outputs":[{"name":"","type":"int256[10]"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"id","type":"uint256"},{"name":"wallet","type":"address"}],"name":"confirm","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"id","type":"uint256"},{"name":"reciever","type":"address"},{"name":"summ","type":"uint256"},{"name":"clientData","type":"string"},{"name":"payers","type":"string"},{"name":"wallets","type":"address[10]"},{"name":"amounts","type":"int256[10]"},{"name":"payStatuses","type":"int256[10]"}],"name":"saveOpenBill","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"id","type":"uint256"}],"name":"getWallets","outputs":[{"name":"","type":"address[10]"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"id","type":"uint256"},{"name":"wallets","type":"address[10]"},{"name":"amounts","type":"int256[10]"}],"name":"setClosing","outputs":[],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"billId","type":"uint256"}],"name":"Closed","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"billId","type":"uint256"}],"name":"Saved","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"billId","type":"uint256"}],"name":"Dead","type":"event"}];
+const contractAddress = "0x585bfca6b761bb6ce91a8dab5255cbec8c485f26";
 const billerContract = web3.eth.contract(abi).at(contractAddress);
 
-// тестовый набор биллей
-let bills = [
-  {
-    id: 1,
-    type: "open",
-    clientData: "",
-    summ: 13,
-    payers: ["0x75dCd745c79Ba87cE88C328b887Ee8A082B7c109", "0x49f2C47f1470989f4E0E8E3aa482f160A76Ee2Aa"],
-    receiver: "0x1d7ED9A6f43C697d365715759f3281C0bc67EcfC"
-  },
-  {
-    id: 2,
-    type: "open",
-    clientData: "",
-    summ: 7,
-    payers: ["0x1d7ED9A6f43C697d36571575", "0x75dCd745c79Ba87cE88C328b887Ee8A082B7c109"],
-    receiver: "0x49f2C47f1470989f4E0E8E3aa482f160A76Ee2Aa"
-  },
-  {
-    id: 3,
-    summ: 13,
-    type: "closing",
-    clientData: "",
-    payers: ["0x1d7ED9A6f43C697d365715759f3281C0bc67EcfC", "0x75dCd745c79Ba87cE88C328b887Ee8A082B7c109"],
-    receiver: "0x49f2C47f1470989f4E0E8E3aa482f160A76Ee2Aa",
-    pays: [
-      {
-        wallet: "0x1d7ED9A6f43C697d365715759f3281C0bc67EcfC",
-        amount: 8,
-        confirmed: false
-      },
-      {
-        wallet: "0x75dCd745c79Ba87cE88C328b887Ee8A082B7c109",
-        amount: 5,
-        confirmed: true
-      }
-    ]
-  }
-];
+billerContract.Saved(function(error, result) {
+  console.log("Saved transaction");
+  if (!error)
+    console.log(result);
+});
+
+billerContract.Closed(function(error, result) {
+  console.log("Closed");
+  if (!error)
+    console.log(result);
+});
+
+billerContract.Dead(function(error, result) {
+  console.log("Completed bill");
+  if (!error)
+    console.log(result);
+});
 
 
 /**
@@ -59,45 +37,31 @@ let bills = [
  */
 exports.saveOpenBill = (bill) => {
   web3.personal.unlockAccount(web3.eth.accounts[0], "password");
-  billerContract.saveOpenBill.sendTransaction(bill.id, bill.receiver, bill.summ, bill.clientData, bill.payers.join(), [], [], {from: web3.eth.accounts[0], gas:1000000});
+
+  billerContract.saveOpenBill.sendTransaction(bill.id, bill.receiver,
+    bill.summ, JSON.stringify(bill.clientData), bill.payers.join(),[],[],[],{from: web3.eth.accounts[0], gas:1000000});
 };
 
 /**
  * Сменить статус контракта на закрывающийся
  */
 exports.closeOpenBill = (bill) => {
-  this.saveOpenBill(bill);
-  // по успеху - пока ждем сидим
   let b = this.getBillById(bill.id);
   if (b.type === 'open') {
-    let partitions = this.getPartition(bill.clientData); // получаем разбивку по людям
-    let s = 0;
-    for (let ind in partitions) {
-      s += partitions[ind].amount;
-      if(b.payers.indexOf(partitions[ind].wallet) > -1) return {result: 'error', message: "payer not from payers"}; // в этой
-    }
-
-    if (s === b.summ) { // разбивка корректная можно посылать на контракт закрытие
-      let wallets = partitions.map((part) => {return part.wallet;});
-      let amounts = partitions.map((part) => {return part.amount;});
-      billerContract.setClosing(bill.id, wallets, amounts); // пишем в блокчейн кто сколько будет платить
-      // по исполнении делаем оповещения всех ребят
-      return {result: "success"}
-    }
-    else {
-      return {result: 'error', message: "сумма не сошлась"};
-    }
-  }
-  else {
-    // по идее ничего не делать
+    let partitions = bill.clientData.payers; // получаем разбивку по людям
+    let wallets = partitions.map((part) => {return part.wallet;});
+    let amounts = partitions.map((part) => {return part.amount;});
+    web3.personal.unlockAccount(web3.eth.accounts[0], "password");
+    billerContract.setClosing.sendTransaction(bill.id, wallets, amounts, {from: web3.eth.accounts[0], gas:1000000}); // пишем в блокчейн кто сколько будет платить
   }
 };
 
 /**
  * Подтвердить свое участие в оплате по счету
  */
-exports.confirmPayment = (billId, accountId) => {
-  // даст команду кошельку accountId сделать перевод на контракт в честь счета с billId
+exports.confirmPayment = (billId, wallet) => {
+  web3.personal.unlockAccount(web3.eth.accounts[0], "password");
+  billerContract.confirm.sendTransaction(parseInt(billId), wallet, {from: web3.eth.accounts[0], gas:1000000});
 };
 
 /**
@@ -115,17 +79,11 @@ exports.getBillsByAccount = (accountWallet) => {
  * получить счет по id
  */
 exports.getBillById = (billId) => {
-  return this.getAllBills().filter((bill) => {return bill.id === billId;})[0];
-};
-
-exports.getMainAccBalance = function test() {
-  return web3.eth.getBalance(web3.eth.coinbase).toNumber();
+  let result =  this.getAllBills().filter((bill) => {return bill.id == billId;})[0];
+  return result;
 };
 
 exports.getAllBills = () => {
-
-  return bills;
-
   web3.personal.unlockAccount(web3.eth.accounts[0], "password");
   let result = [];
   for(let i = 0; i < 20; i++) {
@@ -143,17 +101,35 @@ exports.getAllBills = () => {
       let b = {
         id: i,
         type: type,
-        clientData: bill[3],
+        clientData: JSON.parse(bill[3]),
         summ: parseInt(bill[2].toString(10)),
         payers: bill[4].split(','),
         receiver: bill[1]
       };
 
+      if(type != "open") {
+        let amounts = billerContract.getAmounts(i);
+        let wallets = billerContract.getWallets(i);
+        let payStatuses = billerContract.getPayStatuses(i);
+
+        let payers = [];
+        for(let i =0; i < 10; i++) {
+          if(amounts[i] > 0) {
+            let confirmed = payStatuses[i] == 2;
+            payers.push({
+              amount: amounts[i],
+              wallet: wallets[i],
+              confirmed: confirmed
+            });
+          }
+        }
+        b.payers = payers;
+      }
+
       result.push(b);
     }
   }
   return result;
-
 };
 
 exports.getPartition = (clientData) => {
